cfacb862aaeb1e60d3856f1cfe83556c9919c761
v1km <vikasmali1498@gmail.com>
2018-09-04 19:23:01 +0530

    Add files via upload

---
+++ ARduino_solartracker/ARduino_solartracker.ino
@@ -0,0 +1,82 @@
+#include <Servo.h>
+Servo servohori;
+int servoh = 0;
+int servohLimitHigh = 160;
+int servohLimitLow = 20;
+
+Servo servoverti; 
+int servov = 0; 
+int servovLimitHigh = 160;
+int servovLimitLow = 20;
+int ldrtopl = 2; 
+int ldrtopr = 1; 
+int ldrbotl = 3; 
+int ldrbotr = 0; 
+
+ void setup () 
+ {servohori.attach(10);
+  servohori.write(0);
+  servoverti.attach(9);
+  servoverti.write(0);
+  delay(500);
+  }
+void loop()
+{
+  servoh = servohori.read();
+  servov = servoverti.read();
+  int topl = analogRead(ldrtopl);
+  int topr = analogRead(ldrtopr);
+  int botl = analogRead(ldrbotl);
+  int botr = analogRead(ldrbotr);
+  int avgtop = (topl + topr) / 2; 
+  int avgbot = (botl + botr) / 2; 
+  int avgleft = (topl + botl) / 2; 
+  int avgright = (topr + botr) / 2; 
+
+  if (avgtop < avgbot)
+  {
+    servoverti.write(servov +1);
+    if (servov > servovLimitHigh) 
+     { 
+      servov = servovLimitHigh;
+     }
+    delay(10);
+  }
+  else if (avgbot < avgtop)
+  {
+    servoverti.write(servov -1);
+    if (servov < servovLimitLow)
+  {
+    servov = servovLimitLow;
+  }
+    delay(10);
+  }
+  else 
+  {
+    servoverti.write(servov);
+  }
+  
+  if (avgleft > avgright)
+  {
+    servohori.write(servoh +1);
+    if (servoh > servohLimitHigh)
+    {
+    servoh = servohLimitHigh;
+    }
+    delay(10);
+  }
+  else if (avgright > avgleft)
+  {
+    servohori.write(servoh -1);
+    if (servoh < servohLimitLow)
+     {
+     servoh = servohLimitLow;
+     }
+    delay(10);
+  }
+  else 
+  {
+    servohori.write(servoh);
+  }
+  delay(50);
+}
---
+++ ARduino_solartracker/readme.txt
@@ -0,0 +1,2 @@
+THE circuit diagram is given in jpeg picture and code is also given.For detailed information with pictures visit electronicshub.org
+if further queries:vikasmali14@yahoo.com
